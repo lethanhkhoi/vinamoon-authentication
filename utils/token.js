61bd405e018c2e80da783456addf03c2e8e841d7
Thanh Hoang <thanhhoang4869@gmail.com>
2022-07-22 23:31:22 +0700

    fix expire time

--- utils/token.js
+++ utils/token.js
@@ -1,26 +1,30 @@
-const jwt = require("jsonwebtoken")
-const config = require("../config/app.json")
-const createSecretKey =async (payload, refreshToken = null)=>{
-    const options ={
-        expiresIn: 30,
-        refreshToken: 180,
-    }
-    const token = {
-        token: jwt.sign(payload, config.tokenKey, {expiresIn: options.expiresIn}),
-        refreshToken: refreshToken ?? jwt.sign(payload, config.refreshTokenKey, {expiresIn: options.refreshToken})
-    } 
-    return token
-}
-const decodeToken = async(token)=>{
-    return jwt.verify(token, config.tokenKey)
-}
+const jwt = require("jsonwebtoken");
+const config = require("../config/app.json");
+const createSecretKey = async (payload, refreshToken = null) => {
+  const options = {
+    expiresIn: 60 * 60 * 0.5,
+    refreshToken: 60 * 60 * 24 * 7,
+  };
+  const token = {
+    token: jwt.sign(payload, config.tokenKey, { expiresIn: options.expiresIn }),
+    refreshToken:
+      refreshToken ??
+      jwt.sign(payload, config.refreshTokenKey, {
+        expiresIn: options.refreshToken,
+      }),
+  };
+  return token;
+};
+const decodeToken = async (token) => {
+  return jwt.verify(token, config.tokenKey);
+};
 
-const decodeRefreshToken = async(token)=>{
-    return jwt.verify(token, config.refreshTokenKey)
-}
+const decodeRefreshToken = async (token) => {
+  return jwt.verify(token, config.refreshTokenKey);
+};
 
-module.exports={
-    createSecretKey,
-    decodeToken,
-    decodeRefreshToken
-}
+module.exports = {
+  createSecretKey,
+  decodeToken,
+  decodeRefreshToken,
+};
